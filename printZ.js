52aad14093f6acf012576964cc40a1dfb7f57260
anmolbansal08 <mailmeanmolbansal@gmail.com>
2025-06-01 15:36:16 +0530

    printZ

---
+++ printZ.js
@@ -0,0 +1,15 @@
+const n = 5;
+
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        if(i==0)
+        process.stdout.write('*');
+        else if(i==n-1)
+        process.stdout.write('*');
+        else  if(i+j==n-1)
+        process.stdout.write('*');
+        else process.stdout.write(' ');
+    }
+    console.log();
+}
