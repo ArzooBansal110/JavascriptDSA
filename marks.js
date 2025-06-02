ed2d5d309f8b5a8e97e6a139a6e3d1a9876d1f5a
anmolbansal08 <mailmeanmolbansal@gmail.com>
2025-06-02 22:17:53 +0530

    Marks evaluation
    marks

---
+++ marks.js
@@ -0,0 +1,20 @@
+let marks=undefined;
+if(marks>90){
+    console.log("excellent");
+}
+else if(marks>80 && marks<90){
+    console.log("Good");
+}
+else if(marks>70 && marks<80){
+    console.log("Fair");
+}
+else if(marks>60 && marks<70){
+    console.log("Meet Ex");
+}
+else if(marks<60){
+    console.log("Below par");
+}
+else{
+    console.log("not eval");
+    
+}
